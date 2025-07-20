e2ed57a5d6e9649e72f4e8e13c864c6431117593
LEAN-arch <jbautistamsc@gmail.com>
2025-07-20 16:29:23 -0700

    Update 6_Deviation_Hub.py

--- pages/6_Deviation_Hub.py
+++ pages/6_Deviation_Hub.py
@@ -1,10 +1,10 @@
 # pages/6_deviation_hub.py
 import streamlit as st
 import pandas as pd
-
+from utils import auth
 # Import from the centralized backend modules
 from utils import ui_components, data_connector as dc, auth
-
+auth.check_page_authorization()
 # Set the page configuration for this specific page
 st.set_page_config(page_title="Deviation Hub", page_icon="📌", layout="wide")
 
