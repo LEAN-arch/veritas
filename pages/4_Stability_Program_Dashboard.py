720b6da86544936302bddd19b51cf5e327c2b03f
LEAN-arch <jbautistamsc@gmail.com>
2025-07-20 16:28:39 -0700

    Update 4_Stability_Program_Dashboard.py

--- pages/4_Stability_Program_Dashboard.py
+++ pages/4_Stability_Program_Dashboard.py
@@ -1,10 +1,10 @@
 # pages/4_stability_program_dashboard.py
 import streamlit as st
 import pandas as pd
-
+from utils import auth
 # Import from the centralized backend modules
 from utils import data_connector as dc, plotters, analysis_engine, auth
-
+auth.check_page_authorization()
 # Set the page configuration for this specific page
 st.set_page_config(page_title="Stability Dashboard", page_icon="⏳", layout="wide")
 
